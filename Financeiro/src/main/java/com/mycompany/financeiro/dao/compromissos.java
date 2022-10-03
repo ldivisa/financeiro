d8ba4e917a451745eaca889ee5832998a1e4dbe7
luiz <luiz@luiz-ThinkCentre-M57>
2022-10-03 14:27:16 -0300

    03102022 1427

---
+++ Financeiro/src/main/java/com/mycompany/financeiro/dao/compromissos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.financeiro.dao;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author luiz
+ */
+public class compromissos {
+    
+private int idCompromissos;
+private Date dataCadastro;
+private Date dataVencimento;
+private Double valor;
+private int idFornecedor;
+private String Documento;
+private String parcela;
+private String codigodeBarras;
+private String observacao;
+int tipoDespesa;
+Date dataPagamento
+boolean pagamentoEfetuado;        
+}
