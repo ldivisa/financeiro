90ae9926d71bfd3554664c3122cce7997c9045b4
luiz <luiz@luiz-ThinkCentre-M57>
2022-10-04 19:57:38 -0300

    04102022 2000

---
+++ Financeiro/src/main/java/com/mycompany/financeiro/dao/utilidades/configurações.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.financeiro.dao.utilidades;
+
+import static com.sun.tools.doclint.Entity.prop;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Parameter;
+import java.util.Properties;
+
+/**
+ *
+ * @author luiz
+ */
+public class configurações {
+    public String url;
+    public Properties prop = new Properties();
+
+    
+    
+    public void gravaBanco(String url){
+        
+
+        try {
+        //definir as propriedades e valores
+        prop.setProperty("banco", url);
+        //salvar o arquivo
+        prop.store(new FileOutputStream("config.properties"), null);
+        } catch (IOException ex) {
+        ex.printStackTrace();
+        }    
+         }
+    
+    public Properties leBanco (){
+    try {
+    //carrega properties
+    prop.load(new FileInputStream("config.properties"));
+    } catch (IOException ex) {
+    ex.printStackTrace();
+    }
+    return prop;   
+    }
+
+}
