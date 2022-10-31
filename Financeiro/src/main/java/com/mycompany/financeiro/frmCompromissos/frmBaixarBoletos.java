2843f58a7e135cc8723e5c5d66048c12bb6ef518
luiz <luiz@luiz-ThinkCentre-M57>
2022-10-31 17:42:20 -0300

    31102022 1711

--- Financeiro/src/main/java/com/mycompany/financeiro/frmCompromissos/frmBaixarBoletos.java
+++ Financeiro/src/main/java/com/mycompany/financeiro/frmCompromissos/frmBaixarBoletos.java
@@ -984,9 +984,9 @@ public class frmBaixarBoletos extends javax.swing.JFrame {
     rsCompromissos=null;    
     conexao con = new conexao();
     con.conecta();
-    SQL  = "select * from compromissos,substr(c.DataVencimento, 7, 4) AS ano," +
+    SQL  = "select * from compromissos ,substr(c.DataVencimento, 7, 4) AS ano," +
     "substr(c.DataVencimento, 4, 2) AS mes," +
-    "substr(c.DataVencimento, 1, 2) AS dia  where PagamentoEfetuado = '0' and TipoDivida='0' order by DataVencimento order by ano,mes,dia";
+    "substr(c.DataVencimento, 1, 2) AS dia  from compromissos where PagamentoEfetuado = '0' and TipoDivida='0' order by ano,mes,dia";
     rsCompromissos = con.executaConsulta(SQL);
     compromissos.clear();
     
